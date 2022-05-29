b8687ced403977771e380fd9b9ec8fe50e96829f
ShowoffTH <95130098+Idklol55@users.noreply.github.com>
2022-05-29 10:12:18 +0700

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -9,7 +9,7 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
-import openfl.display.StageScaleMode;
+//import openfl.display.StageScaleMode;
 import lime.system.System;
 
 class Main extends Sprite
@@ -74,10 +74,7 @@ class Main extends Sprite
 		initialState = TitleState;
 		#end
 	
-		ClientPrefs.loadDefaultKeys();
-		// fuck you, persistent caching stays ON during sex
-		FlxGraphic.defaultPersist = true;
-		// the reason for this is we're going to be handling our own cache smartly
+		ClientPrefs.startControls();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		//#if !mobile
